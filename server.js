4964e027379abe66188f6e69c6a71661609423d7
Steven Inouye <steveninouye@msn.com>
2018-10-19 03:08:10 -0700

    starting to connect to mongoose server

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
 const schema = require('./schema/schema');
 
 const app = express();
+const username = 'steveninouye';
+const password = 'test123';
 
-// bind express with graphql
+mongoose.connect(
+    `mongodb://${username}:${password}@ds137003.mlab.com:37003/gql-ninja`
+);
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+});
+
 app.use(
     '/graphql',
     graphqlHTTP({
