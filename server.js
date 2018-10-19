93c57cbf57900ba3935f6dc6758fce7c2dc83eeb
Steven Inouye <steveninouye@msn.com>
2018-10-18 23:46:53 -0700

    added schema to server

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 const PORT = 4000;
 
-app.use('/graphql', graphqlHTTP({}));
+app.use('/graphql', graphqlHTTP({ schema: schema }));
 
 app.listen(PORT, () => console.log(`Server listening on PORT::${PORT}`));
